8c296613bd6712714ece151e1d2ed467da3d81c7
Nthing <store.nthing@gmail.com>
2022-05-15 19:51:04 -0300

    Button and rout Movimentação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [
     AppComponent,
